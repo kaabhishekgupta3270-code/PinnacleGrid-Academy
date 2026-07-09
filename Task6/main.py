80fcc0bfcfb4de0218484a884e56120f90085be8
kaabhishekgupta3270-code <kaabhishekgupta3270@gmail.com>
2026-07-09 22:38:52 +0530

    Added main.py
    Implement a basic employee management system with options to add, view, search, update, and delete employees.

---
+++ Task6/main.py
@@ -0,0 +1,35 @@
+from operations import *
+
+while True:
+
+    print("\n========== Employee Management System ==========")
+    print("1. Add Employee")
+    print("2. View All Employees")
+    print("3. Search Employee")
+    print("4. Update Employee")
+    print("5. Delete Employee")
+    print("6. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        add_employee()
+
+    elif choice == "2":
+        view_employees()
+
+    elif choice == "3":
+        search_employee()
+
+    elif choice == "4":
+        update_employee()
+
+    elif choice == "5":
+        delete_employee()
+
+    elif choice == "6":
+        print("Thank You!")
+        break
+
+    else:
+        print("Invalid Choice!")
